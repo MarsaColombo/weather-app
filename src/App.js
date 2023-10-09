ef5546295aa024009b3b8b7bb3498f6fb90a8d01
Aymeric Biaou <marsa@Aymerics-MacBook-Air.local>
2023-10-09 14:28:21 +0200

    Resolving Manifest Problem

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import logo from "./logo.svg";
 import "./App.css";
 import Input from "./components/Input";
 
+// import axios from 'axios';
 function App() {
-  return <div> </div>;
+  return <div className="App"></div>;
 }
 
 export default App;
